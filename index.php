edfa4216715ed07a80b76e45d3226ea9b8eb93c3
yubathom <yubathom@gmail.com>
2018-08-03 18:35:59 -0300

    Resolve CORS

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Content-type: application/json");
 $monitoramento = json_decode(file_get_contents("output/monitoramento.json")); 
 $hiperlinks = json_decode(file_get_contents("output/hiperlinks.json")); 
 
@@ -6,7 +8,5 @@ $out = new stdClass();
 $out->monitoramento = $monitoramento;
 $out->hiperlinks = $hiperlinks;
 
-
-header("Content-type: application/json");
 echo json_encode($out);
 ?>

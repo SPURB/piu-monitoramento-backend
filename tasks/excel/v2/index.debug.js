75b6ab85e01497bf8f7926281f1e48aafe8fea8d
yubathom <yubathom@gmail.com>
2020-06-16 12:30:06 -0300

    content: revisao

--- tasks/excel/v2/index.debug.js
+++ tasks/excel/v2/index.debug.js
@@ -1,10 +1,16 @@
+import models from './models'
+import { convertModel } from '../builders'
 console.log('------------------ debug ---------------------------')
+console.log(models[4].table)
+
 const datas = convertModel('input/piu-monitoramento_v2.xlsx', models[4])
-console.log('')
-console.log('------------------ nacoes-unidas -------------------')
-console.log(datas.filter(data=>data.id_projetos === 10 && data.id_tramitacao === 1))
 
 console.log('')
 console.log('')
 console.log('------------------ capelinha ---------------------')
 console.log(datas.filter(data=>data.id_projetos === 18 && data.id_tramitacao === 1))
+
+console.log('')
+console.log('')
+console.log('------------------ nações unidas ---------------------')
+console.log(datas.filter(data=>data.id_projetos === 10 && data.id_tramitacao === 3))
